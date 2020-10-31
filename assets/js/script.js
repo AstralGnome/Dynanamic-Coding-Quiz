bd6fde1805c0fc1b3f5a59c9f099019e49f05302
Astral Gnome <reedkeenan@gmail.com>
2020-10-30 22:32:05 -0700

    My struggle has begun. Things are looking bleak.

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+// var quizContainer = document.querySelector("#container");
+
+
+// var startingScore = 75;
+// var totalSeconds = 0;
+// var secondsElapsed =0;
+// var status = "Working";
+// var interval;
+
+questionElem = document.querySelector("#questions");
+
+var questions = [
+    {
+("Velocity?", "Color?", "Currency?");
+
+
+questionElem.append
+
+function startGame () {
+
+    
+}
+
+function displayQuestion()
