38dcf1e5dfb37512cc3eaaf52cf55981887a6132
Astral Gnome <reedkeenan@gmail.com>
2020-11-02 15:11:41 -0800

    added a simple timer to the start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
 var questionEl = document.getElementById("questions");
-var velocityAns = document.getElementById("velocityA");
-var colorAns = document.getElementById("colorA");
-var currencyAns = document.getElementById("currencyA");
+// var velocityAns = document.getElementById("velocityA");
+// var colorAns = document.getElementById("colorA");
+// var currencyAns = document.getElementById("currencyA");
 var startButtonEl = document.getElementById("start");
 var nextButtonEl = document.getElementById("next");
-var totalSeconds = document.getElementById("seconds");
+var timeEl = document.querySelector(".time");
 var interval;
 var questionCount = 0;
 
-
 var questions = [
   ["Velocity?", "Weight", "Height", "Speed", "Speed"],
-  ["Color?", "White", "Blue", "Black", "Blue"],
-  ["Currency?", "Dollar Bills", "Shells", "Dabloons", "Dollar Bills" ]
+  ["Color?", "White", "Black", "Blue", "Blue"],
+  ["Currency?", "Shells", "Dabloons", "Dollar Bills", "Dollar Bills"]
 ];
 
 //This function should create a single question.
 
-//Start should display questions and answers.
+//This should display questions and answers.
 function displayQuestion(questionNum) {
 
     questionEl.innerHTML = `<h1>${questions[questionNum][0]}</h1>
@@ -27,7 +26,10 @@ function displayQuestion(questionNum) {
 
 //add variable to target answers. attach event listers to answer elements.
 
-    var firstAns = document.getElementById("a1");
+    var velocityWrong;
+    velocityWrong = questions[0][1, 2, 3].innerText;
+
+    console.log(velocityAns);
     var secondAns = document.getElementById("a2");
     var thirdAns = document.getElementById("a3");
 
@@ -37,6 +39,22 @@ function displayQuestion(questionNum) {
 //Display next question.
 
 }
+
+var secondsLeft = 100;
+
+function startTimer() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
 
 // setInterval() {
 //     totalSeconds = 100;
@@ -54,6 +72,7 @@ function displayQuestion(questionNum) {
 
 // function startGame () {
     startButtonEl.addEventListener("click", function(){
+        startTimer();
         displayQuestion(questionCount);
     });
 
